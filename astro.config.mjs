1807790cae269d2746832c4dbb4833af41d08c80
Piero Trigoso <trigosop@gmail.com>
2024-07-21 19:53:08 -0300

    chore: update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-sanity.netlify.app",
+  site: "https://luganoconecta.app/",
   integrations: [
     tailwind(),
     sanity({
