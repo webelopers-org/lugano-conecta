b94646fa052524069326630d870e1eb0dd23bd76
Piero Trigoso <trigosop@gmail.com>
2024-07-21 02:11:13 -0300

    chore: deploy settings

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import sanity from "@sanity/astro";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   integrations: [
     tailwind(),
     sanity({
@@ -15,4 +15,6 @@ export default defineConfig({
       useCdn: false,
     }),
   ],
+  output: "server",
+  adapter: netlify(),
 });
